77ac0c64b24608f17bdf7764658be479a897e4f6
Wilford <wilford@smartwebdesign.co.za>
2022-07-27 12:36:44 +0200

    added a createview

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 # Create your views here.
 
@@ -9,6 +10,12 @@ class BlogListView(ListView):
     template_name = "home.html"
 
 
-class BlogDetailView(DeleteView):
+class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
